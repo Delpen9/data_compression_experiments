de821d52b7d476b4eb26f674df3ee3544f112d2c
ian.dover <ian.dover@skunova.com>
2023-04-15 20:33:56 +0000

    almost finishe problem 2

--- problem2/Problem2.py
+++ problem2/Problem2.py
@@ -14,6 +14,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 def PFBS(
+    X0 : np.ndarray,
     X : np.ndarray,
     A : np.ndarray,
     m : int,
@@ -21,31 +22,32 @@ def PFBS(
     n2 : int,
     tau : int = 250,
     max_iteration : int = 500
-) -> np.ndarray:
+) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
     '''
     '''
-    X0 = X.copy()
-
     Y = np.zeros((n1, n2))
     delta = n1 * n2 / m
 
     vec = np.zeros((max_iteration,))
     err = vec.copy()
     for i in range(max_iteration):
-        U, S, V = np.linalg.svd(A)
+        U, S, V = np.linalg.svd(Y)
+        S_diag = np.zeros(Y.shape)
+        S_diag[np.diag_indices(Y.shape[0])] = S
 
-        S_t = (S - tau)
+        S_t = (S_diag - tau)
         S_t[S_t < 0] = 0
 
-        Z = U @ S_t @ V.T
+        Z = U @ S_t @ V
         P = X - Z
+        P[A] = 0
 
         Y0 = Y.copy()
         Y = Y0 + delta * P
 
         vec[i] = np.sum(np.sum((Y - Y0)**2))
         err[i] = np.sum(np.sum((X0 - Z)**2)) / np.sum(np.sum((X0)**2))
-    return err
+    return (Z, err, vec)
 
 if __name__ == '__main__':
     np.random.seed(1234)
@@ -54,9 +56,11 @@ if __name__ == '__main__':
     file_directory = os.path.abspath(os.path.join(current_path, '..', '..', 'data', 'Image.mat'))
     X = scipy.io.loadmat(file_directory)['X']
 
-    A = np.random.rand(*X.shape) >= 0.80
+    X0 = X.copy()
+
+    A = np.random.rand(*X.shape) >= 0.85
     X[A] = 0
     m = np.sum(np.sum(A == 0))
 
-    error = PFBS(X, A, m, *X.shape)
+    Z, error, vectors = PFBS(X0, X, A, m, *X.shape)
     print(error)
