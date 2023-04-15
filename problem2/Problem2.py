92ff15af2e1d3025f1043e441c0e341b10d556c3
ian.dover <ian.dover@skunova.com>
2023-04-15 20:05:08 +0000

    got the algorithm initially coded

--- problem2/Problem2.py
+++ problem2/Problem2.py
@@ -13,6 +13,40 @@ import scipy.io
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+def PFBS(
+    X : np.ndarray,
+    A : np.ndarray,
+    m : int,
+    n1 : int,
+    n2 : int,
+    tau : int = 250,
+    max_iteration : int = 500
+) -> np.ndarray:
+    '''
+    '''
+    X0 = X.copy()
+
+    Y = np.zeros((n1, n2))
+    delta = n1 * n2 / m
+
+    vec = np.zeros((max_iteration,))
+    err = vec.copy()
+    for i in range(max_iteration):
+        U, S, V = np.linalg.svd(A)
+
+        S_t = (S - tau)
+        S_t[S_t < 0] = 0
+
+        Z = U @ S_t @ V.T
+        P = X - Z
+
+        Y0 = Y.copy()
+        Y = Y0 + delta * P
+
+        vec[i] = np.sum(np.sum((Y - Y0)**2))
+        err[i] = np.sum(np.sum((X0 - Z)**2)) / np.sum(np.sum((X0)**2))
+    return err
+
 if __name__ == '__main__':
     np.random.seed(1234)
 
@@ -20,13 +54,9 @@ if __name__ == '__main__':
     file_directory = os.path.abspath(os.path.join(current_path, '..', '..', 'data', 'Image.mat'))
     X = scipy.io.loadmat(file_directory)['X']
 
-    # Make 15% of the pixels = 0
-    zero_count = int(X.flatten().shape[0] * 0.15)
-    indices = np.random.choice(X.flatten().shape[0], size = zero_count, replace = False)
+    A = np.random.rand(*X.shape) >= 0.80
+    X[A] = 0
+    m = np.sum(np.sum(A == 0))
 
-    shape = X.shape
-    X = X.flatten()
-    X[indices] = 0
-    X = X.reshape(shape)
-
-    print(X)
+    error = PFBS(X, A, m, *X.shape)
+    print(error)
