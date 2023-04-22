f992cf0925f683f1fd375c99369d02756ac91898
ian.dover <ian.dover@skunova.com>
2023-04-22 17:43:19 +0000

    in a decent spot on the homework

--- problem4/Problem4.py
+++ problem4/Problem4.py
@@ -19,9 +19,12 @@ import seaborn as sns
 # Saving Images
 import cv2
 
+# Scipy Libraries
+from scipy.spatial.distance import cdist
+
 def gaussian_kernel_1d(
     x : np.ndarray,
-    bandwidth : int = 1
+    bandwidth : int = 1.0
 ) -> np.ndarray:
     '''
     '''
@@ -31,17 +34,10 @@ def gram_matrix(
 )-> np.ndarray:
     '''
     '''
-    x = np.linspace(0, 10, 100)
+    X = np.linspace(0, 10, 100).reshape(-1, 1)
+    distances = cdist(X, X, metric = 'euclidean')
 
-    def squared_euclidean_distance(x, y):
-        return np.sum((x - y)**2)
-
-    distances = np.zeros((100, 100))
-    for i in range(100):
-        for j in range(100):
-            distances[i, j] = squared_euclidean_distance(x[i], x[j])
-
-    kernel_1d = gaussian_kernel_1d(distances, bandwidth = 1)
+    kernel_1d = gaussian_kernel_1d(distances)
 
     gram_matrix = np.kron(kernel_1d, kernel_1d).reshape(10000, 10000)
     return gram_matrix
@@ -51,9 +47,9 @@ def RKHS(
 ) -> np.ndarray:
     '''
     '''
-    _lambda = 0.9
+    _lambda = 0.1
     K = gram_matrix()
-    _alpha = np.linalg.inv(K + _lambda) @ y
+    _alpha = np.linalg.inv(K + _lambda).T @ y
     y_hat = K @ _alpha
     return y_hat
 
@@ -64,15 +60,13 @@ if __name__ == '__main__':
     file_directory = os.path.abspath(os.path.join(current_path, '..', '..', 'data', 'Peaks.mat'))
     y = scipy.io.loadmat(file_directory)['Y']
 
-    y_flattened = y.copy().flatten()
-    y_hat_flattened = RKHS(y_flattened)
-
+    y_hat_flattened = RKHS(y.flatten())
     y_hat = y_hat_flattened.reshape(y.shape)
 
     y_hat = ((y_hat - y_hat.min()) / (y_hat.max() - y_hat.min())) * 255
     file_directory = os.path.abspath(os.path.join(current_path, '..', '..', 'output', 'problem_4_denoised_image.png'))
     cv2.imwrite(file_directory, y_hat)
 
-    y = ((y - y.min()) / (y.max() - y.min())) * 255
-    file_directory = os.path.abspath(os.path.join(current_path, '..', '..', 'output', 'problem_4_image.png'))
-    cv2.imwrite(file_directory, y)
+    # y = ((y - y.min()) / (y.max() - y.min())) * 255
+    # file_directory = os.path.abspath(os.path.join(current_path, '..', '..', 'output', 'problem_4_image.png'))
+    # cv2.imwrite(file_directory, y)
